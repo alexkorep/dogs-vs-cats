ea3c509058cf135beb27eb44d8c870b8beffc050
Alexey Korepanov <alexkorep@gmail.com>
2017-09-10 05:54:57 +0200

    Fix data reading

--- cats_dogs.py
+++ cats_dogs.py
@@ -1,4 +1,40 @@
-from read_koggle_data import read_train_data
+from keras.layers import Flatten, Dense, Conv2D, MaxPooling2D
+from keras.models import Sequential
 
-x_train, y_train, filenames = read_train_data()
-print x_train, y_train
+from read_koggle_data import read_train_data, prepare_train_test_data,\
+    img_rows, img_cols
+
+x_source, y_source, filenames = read_train_data()
+
+(x_train, y_train), (x_test, y_test) = prepare_train_test_data(
+    x_source, y_source, 10000, 1000)
+
+num_classes = 2
+
+# TODO get from prepare_train_test_data because it depends on the backend
+input_shape = (img_rows, img_cols, 1)
+
+print('x_train shape:', x_train.shape)
+print('y_train shape:', y_train.shape)
+print('input_shape shape:', input_shape)
+
+model = Sequential()
+# Add convolution here
+model.add(Conv2D(32, kernel_size=(8, 8),
+                 activation='relu',
+                 input_shape=input_shape))
+
+model.add(MaxPooling2D())
+
+model.add(Conv2D(32, kernel_size=(8, 8), activation='relu'))
+
+model.add(Flatten(input_shape=input_shape))
+model.add(Dense(num_classes, activation='softmax'))
+model.compile(optimizer='rmsprop',
+              loss='mse',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=30, batch_size=32)
+loss_and_metrics = model.evaluate(x_test, y_test, batch_size=16)
+
+#print x_train, y_train
--- read_koggle_data.py
+++ read_koggle_data.py
@@ -1,7 +1,10 @@
 import os
 import cv2
 import numpy as np
+from keras import backend as K
 
+img_rows = 100
+img_cols = 100
 
 def _read_file(filename):
     with open(filename, mode='rb') as file:  # b is important -> binary
@@ -17,15 +20,16 @@ def _read_images(path='data/train'):
     total_files = len(files)
     i = 0
     for file in files:
-        #if i > 1000:
-        #    break
+        # if i > 100:
+        #     break
 
         fullname = path + '/' + file
         im = cv2.imread(fullname)
-        im = cv2.resize(im, (100, 100))
+        im = cv2.resize(im, (img_rows, img_cols))
+        im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
         x.append(im)
-        is_dog = 'dog' in file
-        y.append(is_dog)
+        is_dog = 1 if 'dog' in file else 0
+        y.append([is_dog, 1 - is_dog])
         filenames.append(file)
         i += 1
         if not i%100:
@@ -39,6 +43,12 @@ def save_dump_to_disk(x, y, filenames):
     np.save('data/y.npy', y)
     np.save('data/filenames.npy', filenames)
 
+def reshape_for_backend(x, img_rows, img_cols):
+    if K.image_data_format() == 'channels_first':
+        x = x.reshape(x.shape[0], 1, img_rows, img_cols)
+    else:
+        x = x.reshape(x.shape[0], img_rows, img_cols, 1)
+    return x
 
 def read_train_data():
     try:
@@ -49,5 +59,13 @@ def read_train_data():
         x, y, filenames = _read_images()
         save_dump_to_disk(x, y, filenames)
 
+    x = reshape_for_backend(x, img_rows, img_cols)
+
     return x, y, filenames
 
+def prepare_train_test_data(x_source, y_source, x_size, y_size):
+    x_train = x_source[:x_size]
+    y_train = y_source[:x_size]
+    x_test = x_source[x_size : x_size + y_size]
+    y_test = y_source[x_size : x_size + y_size]
+    return (x_train, y_train), (x_test, y_test)
